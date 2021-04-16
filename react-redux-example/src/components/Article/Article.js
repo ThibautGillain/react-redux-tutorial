97050458c24dc9a6c4aeb50538c59fbab7007490
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2021-04-16 15:40:27 +0200

    return in article

--- react-redux-example/src/components/Article/Article.js
+++ react-redux-example/src/components/Article/Article.js
@@ -2,10 +2,13 @@ import React from "react"
 import "./Article.css"
 
 const article = ({ article }) => {
-    <div className="article">
-        <h1>{article.title}</h1>
-        <p>{article.body}</p>
-    </div>
+    return (
+        <div className="article">
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+        </div>
+    )
+    
 }
 
 export default article;
